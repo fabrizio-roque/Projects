80327ba17642f42d21eccbfd6989517fd6289d93
fabrizio <aroque@correo.um.edu.uy>
2023-12-23 12:10:51 -0300

    Add interactive functionalities

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,24 +1,41 @@
 import string
 import time
 
-word = "Hello World"
-wordComplete = ""
-abc = list(string.ascii_lowercase)
+def generate_letters(word, alphabet):
+    """Generador que produce las letras y caracteres a imprimir para una palabra dada."""
+    for char in word:
+        if char == " ":
+            yield char, None
+            continue
 
-for i in range(len(word)):
-    l = word[i].lower()
-    for letter in abc:
-        if l == letter:
-            time.sleep(0.1)
-            wordComplete += word[i]
+        for letter in alphabet:
+            if char.lower() == letter:
+                yield char, None
+                break
+            else:
+                yield None, letter
+
+def print_word_progression(word, delay=0.04):
+    """Imprime la progresión de la formación de una palabra, letra por letra."""
+    wordComplete = ""
+    for char, letter in generate_letters(word, list(string.ascii_lowercase)):
+        if char:
+            wordComplete += char
             print(wordComplete)
-            break  # Rompe el bucle interior y continúa con el siguiente carácter de 'word'
-        
-        elif word[i] == " ":
-            wordComplete += " "
-            break
-    
+            time.sleep(delay)
         else:
             print(wordComplete + letter)
-            time.sleep(0.1)
+            time.sleep(delay)
+
+def main():
+    """Función principal que solicita una entrada del usuario y llama a la función de impresión."""
+    user_word = input("Ingrese una palabra o frase: ")
+    print_word_progression(user_word)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
 
